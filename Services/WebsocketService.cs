07c22fefe879d2d6726f8147e03354d0847765cb
Marcel Amsler <marcelamsler@Marcels-MacBook-Pro.local>
2015-10-14 18:26:13 +0200

    first commit

---
+++ Services/WebsocketService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using WebSocket.Portable;
+using System.Diagnostics;
+using WebSocket.Portable.Interfaces;
+
+namespace WebsocketTest
+{
+    public class WebsocketService
+    {
+        private WebSocketClient websocketClient;
+
+        public WebsocketService()
+        {
+            websocketClient = new WebSocketClient();
+            websocketClient.Opened += OnWebsocketOpened;
+            websocketClient.Closed += OnWebsocketClosed;
+            websocketClient.MessageReceived += OnWebsocketReceived;
+            websocketClient.Error += OnWebsocketError;
+        }
+
+        private async void ConnectWithWebsocket(string webSocketEndpoint) 
+        {
+            await websocketClient.OpenAsync(webSocketEndpoint);
+        }
+
+        void OnWebsocketOpened()
+        {
+            Debug.WriteLine("Opened Websocket");
+        }
+
+        void OnWebsocketClosed()
+        {
+            Debug.WriteLine("Closed Websocket");
+        }
+
+        void OnWebsocketReceived(IWebSocketMessage obj)
+        {
+            Debug.WriteLine("Websocket Message Received: " + obj.ToString());
+        }
+
+        void OnWebsocketError(Exception e)
+        {
+            Debug.WriteLine("Websocket Error" + e.Message);
+        }
+    }
+}
+
---
+++ SocketTestPage2.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using WebSocket.Portable;
+using System.Threading.Tasks;
+using WebSocket.Portable.Interfaces;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace WebsocketTest
+{
+    public partial class SocketTestPage2 : ContentPage
+    {
+        WebSocketClient client;
+
+        int MessageCount { get; set; }
+
+        int MissingMessages
+        {
+            get;
+            set;
+        }
+
+        List<int> messages = new List<int>();
+
+        public SocketTestPage2()
+        {
+            InitializeComponent();
+
+            client = new WebSocketClient();
+
+            MessageCount = 100;
+        }
+
+
+        private async void OnConnect(object sender, EventArgs e) 
+        {
+            await client.OpenAsync("ws://drallodmmprototype.azurewebsites.net/dmmSocketConnector.ashx");
+
+            Debug.WriteLine("connected");
+        }
+
+        private async void OnSingleMessage(object sender, EventArgs e) 
+        {
+            client.FrameReceived += printMessage;
+
+            await client.SendAsync("{message: 'Bob'}");
+            Debug.WriteLine("send one message");
+        }
+
+        private async void OnMultipleMessages(object sender, EventArgs e) 
+        {
+            client.FrameReceived += printMissingMessages;
+
+            for (int i = 0; i < MessageCount; i++)
+            {
+                await client.SendAsync("{message: '"+ i + "'}");
+                Debug.WriteLine("send a message");
+                messages.Add(i);
+            }
+
+
+                
+        }
+
+        private void printMissingMessages(IWebSocketFrame frame)
+        {
+            var payload = frame.ToString();
+            var indexOfBegin = payload.IndexOf("'");
+            var message = payload.Substring(indexOfBegin + 1, payload.Length - 2 );
+
+            var parsedMessage = JObject.Parse(message);
+            if (messages.Contains(int.Parse(parsedMessage[message].ToString()))) 
+            {
+                    MissingMessages++;
+            }
+            valueLabel.Text = MissingMessages.ToString();                   
+        }
+
+        private void printMessage(IWebSocketFrame frame) {
+            valueLabel.Text = frame.ToString();
+        }
+
+    }
+}
+
