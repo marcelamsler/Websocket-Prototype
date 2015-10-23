0f3d671888950b7a5bf18f0c48fb000e0ca5d259
Daniel Kolb <kolbdaniel1@gmail.com>
2015-10-23 12:01:40 +0200

    added send and receive methods

---
+++ DataObjects/WebsocketEchoMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace WebsocketTest
+{
+	public class WebsocketEchoMessage
+	{
+		[JsonProperty(PropertyName = "timestamp")]
+		public string timestamp { get; set; }
+		[JsonProperty(PropertyName = "message")]
+		public string message{ get; set; }
+		public WebsocketEchoMessage (string serializedJSON)
+		{
+		}
+
+	}
+}
+	
